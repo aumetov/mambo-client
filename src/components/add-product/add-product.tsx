fc46d001193e9784e248b8b2a3ec514669c86b57
Atai <umetov.atai@gmail.com>
2020-05-31 21:02:44 +0600

    add-product input handlers

--- src/components/add-product/add-product.tsx
+++ src/components/add-product/add-product.tsx
@@ -11,24 +11,75 @@ import FormControl from '@material-ui/core/FormControl';
 const addNewProductIcon = require('../../shared/icons/add.png')
 const deleteIcon = require('../../shared/icons/delete.png')
 
-export default class AddProduct extends Component {
+interface IProps {
+}
+
+interface IState {
+    title: string;
+    categories: number[];
+    price: number;
+    salePrice?: number;
+    description?: string;
+    collection?: string;
+    status: string;
+    shopId: string;
+    sizes: string[];
+    sex: string;
+}
+
+export default class AddProduct extends Component<IProps, IState> {
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            title: '',
+            categories: [],
+            price: 0,
+            salePrice: 0,
+            description: '',
+            collection: '',
+            status: '',
+            shopId: '',
+            sizes: [],
+            sex: ''
+        };
+    }
+
     render() {
         return (
             <div className='add-product-container'>
                 <div className='main-product-info'>
                     <div className='main-product-info-textfields'>
-                        <TextField label="Название" className='product-info-input'/>
+                        <TextField label="Название" value={this.state.title} onChange={this.onTitleChange} className='product-info-input'/>
                         <TextField label="Категория" className='product-info-input'/>
-                        <TextField label="Цена" className='product-info-input'/>
-                        <TextField label="Цена распродажи" className='product-info-input'/>
-                        <TextField label="Коллекция" className='product-info-input'/>
-                        <TextField label="Статус" className='product-info-input'/>
+                        <TextField label="Цена" value={this.state.price} onChange={this.onPriceChange} type='number' className='product-info-input'/>
+                        <TextField label="Цена распродажи" value={this.state.salePrice} onChange={this.onSalePriceChange} type='number' className='product-info-input'/>
+                        <TextField label="Коллекция" value={this.state.collection} onChange={this.onCollectionChange} className='product-info-input'/>
+                        <FormControl>
+                            <InputLabel id="status-select-label">Статус</InputLabel>
+                            <Select
+                                labelId="status-select-label"
+                                id="status-select"
+                                value={this.state.status}
+                                onChange={this.onStatusChange}
+                            >
+                                <MenuItem value={'in-stock'}>В наличии</MenuItem>
+                                <MenuItem value={'out-of-stock'}>Нет в наличии</MenuItem>
+                                <MenuItem value={'delivery-expected'}>Ожидается поставка</MenuItem>
+                            </Select>
+                        </FormControl>
                     </div>
                     <div className="product-photos-upload">
 
                     </div>
                 </div>
-                <TextField label="Описание" multiline className='product-description-input'/>
+                <TextField
+                    label="Описание"
+                    multiline rows={4}
+                    className='product-description-input'
+                    value={this.state.description}
+                    onChange={this.onDescriptionChange}
+                    inputProps={{maxLength: 340}}
+                />
                 <div className='product-options'>
                     <FormControl style={{width: 356}}>
                         <InputLabel id="size-selector">Размер</InputLabel>
@@ -71,10 +122,10 @@ export default class AddProduct extends Component {
                     </FormControl>
                 </div>
                 <div className="product-action-buttons">
-                    <button className='delete-product-button'>
+                    {/* <button className='delete-product-button'>
                         <img className='delete-product-icon' alt='delete product' src={deleteIcon}/>
                         Удалить
-                    </button>
+                    </button> */}
 
                     <button className='add-product-button'>
                         <img className='add-product-icon' alt='add product' src={addNewProductIcon}/>
@@ -83,5 +134,29 @@ export default class AddProduct extends Component {
                 </div>
             </div>
         )
+    }
+
+    onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({title: e.target.value})
+    }
+
+    onPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({price: parseInt(e.target.value)})
+    }
+
+    onSalePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({salePrice: parseInt(e.target.value)})
+    }
+
+    onCollectionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({collection: e.target.value})
+    }
+
+    onStatusChange = (e: React.ChangeEvent<any>) => {
+        this.setState({status: e.target.value})
+    }
+
+    onDescriptionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        this.setState({description: e.target.value})
     }
 }
---
+++ src/components/edit-product/edit-product.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './edit-product.scss'
+
+export default class EditProduct extends Component {
+    render() {
+        return (
+            <div className='edit-product-container'>
+                
+            </div>
+        )
+    }
+}
