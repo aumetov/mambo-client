01681322211d3cc7c7d211d327acb2abb0673d8a
Atai <umetov.atai@gmail.com>
2020-06-15 22:48:19 +0600

    sidebar filter to function component and categories request

--- src/components/sidebard-filter/sidebar-filter.tsx
+++ src/components/sidebard-filter/sidebar-filter.tsx
@@ -1,78 +1,108 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import './sidebar-filter.scss'
+import { connect, ConnectedProps } from "react-redux";
 import 'react-input-range/lib/css/index.css'
 import InputRange from 'react-input-range';
+import { getProductsRequest, fetchCategoriesRequest } from '../../actions/actions';
 
-export default class SidebarFilter extends Component {
-    render() {
-        return (
-            <div className='sidebar-filter-container'>
-                <div className='filters'>
-                    <div className='categories-filter'>
-                        <p className='filter-title'>Категории</p>
-                        <p className='category'>Футболки</p>
-                        <p className='category'>Брюки</p>
-                        <p className='category'>Рубашки</p>
-                        <p className='category'>Платья</p>
-                        <p className='category'>Свитера</p>
-                        <p className='category'>Толстовки</p>
-                        <p className='category'>Куртки</p>
-                    </div>
+interface RootState{
+    products: any[],
+    categories: any[]
+}
 
-                    <div className='sex-filter'>
-                        <p className='filter-title'>Пол</p>
-                        <div className='sex-checkboxes'>
-                            <input type="checkbox" id="male" name="male" value=""/>
-                            <label>Мужской</label>
+interface RootDispatch{
+    fetchProducts: (query: string) => void;
+    fetchCategories: () => void;
+}
 
-                            <input type="checkbox" id="female" name="female" value=""/>
-                            <label>Женский</label>
-                        </div>
-                    </div>
+type PropsFromRedux = ConnectedProps<typeof connector>;
+type Props = PropsFromRedux & RootDispatch;
 
-                    <div className='size-filter'>
-                        <p className='filter-title'>Размеры</p>
-                        <div className='sizes'>
-                            <div className='size'/>
-                            <div className='size'/>
-                            <div className='size'/>
-                            <div className='size'/>
-                            <div className='size'/>
-                            <div className='size'/>
-                            <div className='size'/>
-                            <div className='size'/>
-                        </div>
-                    </div>
+const SidebarFilter:React.FC<Props> = ({products, fetchProducts, categories, fetchCategories}:Props) => {
+    useEffect(() => {
+        fetchCategories()
+    }, [fetchCategories])
 
-                    <div className='color-filter'>
-                        <p className='filter-title'>Цвета</p>
-                        <div className='colors'>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                            <div className='color'/>
-                        </div>
-                    </div>
+    return (
+        <div className='sidebar-filter-container'>
+            <div className='filters'>
+                <div className='categories-filter'>
+                    <p className='filter-title'>Категории</p>
+                    {categories.map(category => (
+                        <p className='category'>{category.displayTitle}</p>
+                    ))}
+                </div>
 
-                    <div className='price-filter'>
-                        <p className='filter-title'>Цена</p>
-                        <InputRange
-                            maxValue={100}
-                            minValue={0}
-                            formatLabel={value => `${value} с`}
-                            value={{min:0, max:20}}
-                            onChange={value => console.log(value)}
-                            onChangeComplete={value => console.log(value)}
-                        />
+                <div className='sex-filter'>
+                    <p className='filter-title'>Пол</p>
+                    <div className='sex-checkboxes'>
+                        <input type="checkbox" id="male" name="male" value=""/>
+                        <label>Мужской</label>
+
+                        <input type="checkbox" id="female" name="female" value=""/>
+                        <label>Женский</label>
                     </div>
                 </div>
+
+                <div className='size-filter'>
+                    <p className='filter-title'>Размеры</p>
+                    <div className='sizes'>
+                        <div className='size'/>
+                        <div className='size'/>
+                        <div className='size'/>
+                        <div className='size'/>
+                        <div className='size'/>
+                        <div className='size'/>
+                        <div className='size'/>
+                        <div className='size'/>
+                    </div>
+                </div>
+
+                <div className='color-filter'>
+                    <p className='filter-title'>Цвета</p>
+                    <div className='colors'>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                        <div className='color'/>
+                    </div>
+                </div>
+
+                <div className='price-filter'>
+                    <p className='filter-title'>Цена</p>
+                    <InputRange
+                        maxValue={100}
+                        minValue={0}
+                        formatLabel={value => `${value} с`}
+                        value={{min:0, max:20}}
+                        onChange={value => console.log(value)}
+                        onChangeComplete={value => console.log(value)}
+                    />
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+const mapStateToProps = (state: RootState) => ({
+    products: state.products,
+    categories: state.categories
+});
+
+const mapDispatchToProps:RootDispatch = ({
+    fetchProducts: getProductsRequest,
+    fetchCategories: fetchCategoriesRequest
+});
+
+const connector = connect(
+    mapStateToProps,
+    mapDispatchToProps
+);
+
+export default connector(SidebarFilter);
